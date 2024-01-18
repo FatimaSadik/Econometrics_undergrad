3a846d0b2eebce744e6763ddb24eafa34a45f8a6
FatimaSadik <109474145+FatimaSadik@users.noreply.github.com>
2024-01-18 22:07:45 +0500

    Add files via upload

---
+++ 15.1_JW.R
@@ -0,0 +1,20 @@
+#Estimating the Return to Education for Married Women
+library("wooldridge")
+library(AER)
+data(mroz)
+head(mroz)
+model1<-lm(lwage~educ,data=mroz)
+coefficients(model1)
+summary(model1)
+#we use father’s education (fatheduc) as an instrumental variable 
+#for educ. We have to maintain that fatheduc is uncorrelated with u. 
+#The second requirement is that educ and fatheduc are correlated.
+#We can check this very easily using a simple regression of educ on fatheduc
+model2<-lm(educ~fatheduc, data=mroz)
+summary(model2)
+mroz$educ_hat<-fitted.values(model2)
+model3<-lm(lwage~educ_hat, data=mroz)
+summary(model3)
+iv<-ivreg(lwage~educ|fatheduc, data=mroz)
+summary(iv)
+coeftest(iv,vcov=vcovHC, type="HC1")
